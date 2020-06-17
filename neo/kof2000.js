e67deb51928d39f41933ab7a090637c4deb1d856
bombzj <bombzj@gmail.com>
2020-06-17 14:51:03 +0800

    fix kof2000 sprite palette

--- neo/kof2000.js
+++ neo/kof2000.js
@@ -16,11 +16,10 @@ function loadRomPal() {
 	}
 
 	// load character palette
-
-		bf.position(paletteAddress + palset * 0x400);
-		for(let i = 0;i < 32;i++) {
-			loadRomPalNeo(bf, (i << 4));
-		}
+	bf.position(paletteAddress + palset * 0x400);
+	for(let i = 16;i < 32;i++) {
+		loadRomPalNeo(bf, (i << 4));
+	}
 	
 	if(showPal)
 		drawPal();
@@ -483,9 +482,9 @@ function getRomFrame(addr, f) {
 var animPlayerAddr = [];
 
 var palmap = [
-	8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,
-	50,52,54,56,58,60,62,64,66,
-	68,70,72,74,76,78,80
+	8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,
+	27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,
+	46,47,48,49,50,51,52,53,54
 ];
 
 function loadRomFrame() {
