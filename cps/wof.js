cce1e43deadc65ee84dfd7efcde1b329a7755b31
bombzj <bombzj@gmail.com>
2020-06-20 09:30:01 +0800

    wof background layer

--- cps/wof.js
+++ cps/wof.js
@@ -84,13 +84,66 @@ function drawCB(bf, c = ctx, offx = 128, offy = 160) {
 //	labelInfo.title = 'x=' + x + ',' + x2 + ' y=' + y + ',' + y2 + ' z=' + z + ',' + z2;
 }
 
+var mapAddress = 0x18A86;
 
 let mapWidth = 32;
 let mapHeight;	// default 8
 let mapGrid = 2;		// each map tile contains 4 raw tiles?
 // draw a background with tilemap
 function drawMap() {
+	var bf = new bytebuffer(romFrameData);
+	var bf2 = new bytebuffer(romFrameData);
+	var bf3 = new bytebuffer(romFrameData);
+	ctxBack.clearRect(0, 0, canvasBack.width, canvasBack.height);
+	
+	let tileindex = bf.getInt(mapAddress + curMap * 16 + 4);
+	let tileaddr = bf.getInt(mapAddress + curMap * 16 + 8);
+	let bigindex = bf.getInt(mapAddress + curMap * 16);
+	
+//	labelInfo.innerText = 'address:' + bf.position().toString(16).toUpperCase()
+//			+ ' 2x2tile address:' + mapTileAddress[curMap].toString(16).toUpperCase();
+	var imageData = ctxBack.createImageData(gridWidth, gridHeight);
 
+	var height = 2;
+	bf3.position(bigindex);
+	
+	let startscr=0;
+	for(let scr=0;scr<6 + mapAddressSkip * 2;scr++) {
+		if(scr%2==0)bf3.skip(4)
+		let scrTile = bf3.getShort();
+		if(scr<mapAddressSkip * 2)
+			continue;
+		
+		
+
+		let scrx = Math.floor(startscr / height) * 256;
+		let scry = (height - startscr % height - 1) * 256;
+
+		bf.position(tileindex+scrTile * 64 * 2);
+	
+		for(let i=0;i<8;i++) {
+			for(let j=0;j<8;j++) {
+				let maptile=bf.getShort();
+				bf2.position(maptile*4*4 + tileaddr);
+				for(let gi=0;gi<mapGrid;gi++)
+					for(let gj=0;gj<mapGrid;gj++) {
+						
+						let tile = bf2.getShort();
+						let flag = bf2.get();
+						let pal = bf2.get();
+						if(hideBackground) {	// hide background based on flag and color, 0x10 maybe the switch
+							let hide = flag & 0xF;
+							if((pal & 0x80) == 0)
+								hide = 16;
+							drawTilesBase(imageData, tile, 1, 1, (pal & 0x1F) + 0x40, 16, false, (pal & 0x40), (pal & 0x20), hide);
+						} else 
+							drawTilesBase(imageData, tile, 1, 1, (pal & 0x1F) + 0x40, 16, false, (pal & 0x40), (pal & 0x20));
+						ctxBack.putImageData(imageData, scrx + (i*mapGrid+gi) * gridWidth, scry + (j*mapGrid+gj)%16 * gridHeight);
+					}
+			}
+		}
+		startscr++;
+	}
 }
 
 
