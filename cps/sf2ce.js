8df3794aee9b2732045fbd9430ce1524e3eb0bd4
bombzj <bombzj@gmail.com>
2020-06-27 16:43:25 +0800

    sf2ce sprite

--- cps/sf2ce.js
+++ cps/sf2ce.js
@@ -123,7 +123,7 @@ function drawMap() {
 
 
 var map2Data = [
-	0x2313E,	0x2315C,	0x2317A,	0x23184,	0x231A2,	0x231B6,	0x231D4,	0x231FC
+	
 ];
 let map2Width = 16;
 let map2Height = 8;
@@ -136,10 +136,59 @@ function setMapTileStart(mapstart) {
 	refresh();
 }
 
+frameAddress = [
+	0x728DC, 0x823FA, 0x728F4
+];
 
 //get frame from addr. return a frame obj
 function getRomFrame(addr){
+	var bf = new bytebuffer(romFrameData);
+	var bf2 = new bytebuffer(romFrameData);
+	var bf3 = new bytebuffer(romFrameData);
 
+	let frame = {
+		sprites : []
+	};
+debugger
+	bf.position(addr);
+	let addr2 = bf.getrInt(4);
+	bf2.position(addr2);
+	let cnt = bf2.getShort();
+	let palette = bf2.getShort();
+	let positionIndex = bf2.getShort();
+	let a5_749A = bf2.getShort();
+	let a5_7498 = bf2.getShort();
+	let positionIndex2 = (positionIndex & 0x7F80) >> 5;
+	let addr3 = bf.getInt(0x98112 + positionIndex2);
+
+	addr3 += bf.getShort(addr3 + (positionIndex & 0x7F) * 2);
+	bf3.position(addr3);	// position list
+
+	for(let j = 0;j < cnt;j++) {
+		let tile = bf2.getuShort();
+		if(tile == 0) {
+			j--;
+			bf3.skip(4);
+			continue;
+		}
+		let x = bf3.getShort();
+		let y = bf3.getShort();
+
+		let sprite = {
+			x : x,
+			y : y,
+			tile : tile,
+			nx : 1,
+			ny : 1,
+			vflip : palette & 0x40,	// this tile need flip
+			hflip : palette & 0x20,	// this tile need flip
+			pal : palette & 0x1F,
+		};
+	
+		frame.sprites.push(sprite);
+	}
+
+	return frame;
 }
 
 var romFrames = [];		// frames that extracted from romFrameData
